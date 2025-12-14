495f47cb20cc38a7222a2592effa89b031b2ec07
Kinoti <vickykinoti001@gmail.com>
2025-12-14 22:22:14 +0300

    v0.1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+let burgers = [`Hamburger`, `Cheeseburger`];
+let featuredDrink = `Strawberry milkshake`;
+
+function addBurger() {
+  let newBurger = "flatBurger";
+  burgers.push(newBurger);
+}
+
+if (true) {
+  let anotherBurger = `Maple Bacon Burger`;
+  burgers.push(anotherBurger);
+}
+
+function changeFeaturedDrink() {
+  let featuredDrink = `The JavaShake`;
+}
